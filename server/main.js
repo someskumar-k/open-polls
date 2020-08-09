27625cf81b0bc5dbf1f2ba39fea9d3d90fb903d8
Somes Kumar K <someshkumarasamy@gmail.com>
2020-08-09 23:58:44 +0530

    final

--- server/main.js
+++ server/main.js
@@ -4,25 +4,6 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
-const sslRedirect = function(environments, status) {
-  environments = environments || ['production'];
-  status = status || 302;
-  return function(req, res, next) {
-    if (environments.indexOf(process.env.NODE_ENV) >= 0) {
-      if (req.headers['x-forwarded-proto'] != 'https') {
-        res.redirect(status, 'https://' + req.hostname + req.originalUrl);
-      }
-      else {
-        next();
-      }
-    }
-    else {
-      next();
-    }
-  };
-};
-;
-
 const {initilizeDB} = require('./sqlitedb/sqlite')
 const {isAuthenticated} = require('./middleware/auth')
 const {populateroles,userinrole} = require('./middleware/role')
@@ -34,21 +15,17 @@ const {getAllCandiates,
 
 
 const port = process.env.PORT || 3051
-initilizeDB();
 const dburi = process.env.dburi
 mongoose.connect(dburi, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then(() => console.log('DB Connected Sucessfully'))
   .catch(err => console.log("Error Occurred in DB Connection \n"))
 app.use(cors())
-app.use(sslRedirect());
-
-app.use(populateroles)
 app.use(bodyParser.json({limit:'5mb'}));
 app.use(express.static(path.join(__dirname, 'client')));
 app.options("*",function(req,res,next){
   res.status(200).send()
 })
-app.get('/', async function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'client', 'index.html'));
 })
 app.get('/listcontestants',userinrole('user'),getAllCandiates);
